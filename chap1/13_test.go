c97de7207becacfddb9b7227d494d2e4522a512e
Tarun Verma <tr.vrma@gmail.com>
2024-08-30 22:31:54 +0100

    more exercises

--- chap1/13_test.go
+++ chap1/13_test.go
@@ -1,10 +1,22 @@
-package echo
+package main
 
-import "testing"
+import (
+	"testing"
+	"os"
+)
 
 
-func BenchmarkIsPalindrome(b *testing.B) {
-        for i := 0; i < b.N; i++ {
-                argsWithoutJoin()
+func BenchmarkArgsWithJoin(b *testing.B) {
+	os.Args = []string{"cmd", "arg1", "arg2", "arg3", "arg4", "arg5"}
+	for i := 0; i < b.N; i++ {
+		argsWithJoin()
+    }
+}
+
+
+func BenchmarkArgsWithoutJoin(b *testing.B) {
+	os.Args = []string{"cmd", "arg1", "arg2", "arg3", "arg4", "arg5"}
+	for i := 0; i < b.N; i++ {
+		argsWithoutJoin()
     }
 }
